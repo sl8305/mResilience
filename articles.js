fd5210ec4368fecbe54b0c447c9107089fc5c29b
Marino Carranza <marino.carranza@gmail.com>
2019-08-10 10:20:20 -0700

    latest stuff on articles

--- articles.js
+++ articles.js
@@ -5,7 +5,7 @@
  */
 function buildQueryURL() {
   // queryURL is the url we'll use to query the API
-  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=wellness";
 
   // Begin building an object to contain our API call's query parameters
   // Set the API key
